aa680f3f742faf13d31aa00203ce26ffc777827d
akillirehabplatformu <39339635+akillirehabplatformu@users.noreply.github.com>
2018-09-14 15:24:46 +0300

    Update index.js

--- index.js
+++ index.js
@@ -28,3 +28,40 @@ $(document).ready(function(){
         $(".mob-right").css("right","-150%");
     })
 })
+
+
+$(document).ready(function(){
+    $(".mob-today-div-opener").click(function(){
+        $(".mob-light-div").css("display","none");
+        $(".mob-categories-div").css("display","none");
+        $(".mob-light-div-opener").css("color","white");
+        $(".mob-categories-div-opener").css("color","white");
+        
+        $(".mob-today-div").css("display","block");
+        $(".mob-today-div-opener").css("color","orange");
+    })
+})
+
+$(document).ready(function(){
+    $(".mob-light-div-opener").click(function(){
+        $(".mob-today-div").css("display","none");
+        $(".mob-categories-div").css("display","none");
+        $(".mob-today-div-opener").css("color","white");
+        $(".mob-categories-div-opener").css("color","white");
+        
+        $(".mob-light-div").css("display","block");
+        $(".mob-light-div-opener").css("color","orange");
+    })
+})
+
+$(document).ready(function(){
+    $(".mob-categories-div-opener").click(function(){
+        $(".mob-today-div").css("display","none");
+        $(".mob-light-div").css("display","none");
+        $(".mob-today-div-opener").css("color","white");
+        $(".mob-light-div-opener").css("color","white");
+        
+        $(".mob-categories-div").css("display","block");
+        $(".mob-categories-div-opener").css("color","orange");
+    })
+})
